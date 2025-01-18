02dc9844fa90912e70b4c6e30be9f2ed660d12aa
michaelm96 <michaelarceus96@gmail.com>
2025-01-18 17:12:09 +0800

    feat: add custom invalidation in case want to add a new member with inexist team id

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Member) {
+  // Custom validation before saving a Member
+  Member.observe('before save', function(ctx, next) {
+    const teamId = ctx.instance ? ctx.instance.teamId : ctx.data.teamId;
 
+    if (teamId) {
+      Member.app.models.Team.findById(teamId, function(err, team) {
+        if (err) {
+          return next(err); // Handle any errors in the database query
+        }
+
+        if (!team) {
+          const error = new Error(`Team with id ${teamId} does not exist.`);
+          error.statusCode = 400;
+          return next(error); // Prevent the Member from being saved
+        }
+
+        next(); // Continue with saving the Member if the teamId is valid
+      });
+    } else {
+      next(); // Proceed if no teamId is inexist
+    }
+  });
 };
